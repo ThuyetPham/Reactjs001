3d5f12c44d129834fdbcf816513145b9194628f2
ThuyetPham <thuyetpham2401@gmail.com>
2021-03-15 11:28:32 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -12,14 +12,36 @@ constructor(props){
   super(props) 
   this.state = {
     color: 'red',
-    fontSize: 15
+    fontSize: 12
   };
+  this.onSetColor=this.onSetColor.bind(this);
+  this.onChangeSize=this.onChangeSize.bind(this);
+  this.onSetingDefault=this.onSetingDefault.bind(this);
+
 }
 
-onSetColor(params) {
-  console.log(params)
+onSetColor(params){
+  this.setState({
+    color: params,
+  })
 }
 
+onChangeSize(value){
+  if(this.state.fontSize + value >=8 && this.state.fontSize + value <=36) {
+    this.setState({
+      fontSize : this.state.fontSize + value
+    });
+  }
+}
+
+onSetingDefault(value){
+  if(value){
+    this.setState({
+      color: 'red',
+      fontSize: 12
+    });
+  }
+}
 
   render() {
     return (
@@ -27,10 +49,13 @@ onSetColor(params) {
         <div className='row'>
           <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor}/>
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <SizeSetting/>
-            <Reset/>
+            <SizeSetting 
+            fontSize={this.state.fontSize}
+            onChangeSize={this.onChangeSize}
+            />
+            <Reset onSetingDefault={this.onSetingDefault}/>
           </div>
-          <Result/>
+          <Result color={this.state.color} fontSize={this.state.fontSize}/>
         </div>
       </div>
     )
--- src/component/Reset.js
+++ src/component/Reset.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Reset extends React.Component {
+
+  onResultDefault = () => {
+    this.props.onSetingDefault(true);
+  }
   render() {
     return (
         <>
-            <button type="button" className="btn btn-primary" >reset</button>
+            <button type="button" className="btn btn-primary" onClick={this.onResultDefault}>reset</button>
         </>
     )
   }
